00324dbff075279ba8aa13264980e8eb1659d53b
pranav12060 <pra22.shinde@gmail.com>
2025-07-31 23:04:13 +0530

    updated MongoDB Node driver

--- CollabSpace/devconnect-backend/config/db.js
+++ CollabSpace/devconnect-backend/config/db.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        ssl: true,
-        tlsAllowInvalidCertificates: true
-      });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err);
